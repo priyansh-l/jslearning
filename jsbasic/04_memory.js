3e4ed48bd5528d32f1b330edab7d9987c1ac8992
priyansh-l <121529305+priyansh-l@users.noreply.github.com>
2025-06-05 07:35:43 +0000

    2-6

---
+++ jsbasic/04_memory.js
@@ -0,0 +1,16 @@
+
+//2 types of memories 
+// stack and heap
+//stack ->primitive
+// heap->non primitive
+
+let myName="Priyansh"
+
+let user1={
+    email:"user1@procore.com",
+    upi:"user@ybl"
+}
+let user2=user1;
+user2.email="user2@procore.com"
+console.log(user1.email);
+
